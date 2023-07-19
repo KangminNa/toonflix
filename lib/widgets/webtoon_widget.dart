a3011fe58969eaa7ffa85484213e61494f469f02
nakangmin <139530542+KangminNa@users.noreply.github.com>
2023-07-19 09:33:42 +0900

    feat : Hero, Recap
    - ListView 구현, Webtoon Widget을 구현하여 포스터 및 텍스트
    - Webtoon : 포스터가 포함된 Column, GestureDetector : ontap사용 -> Navigator.push 사용, Material PageRoute를 이용해서 화면 이동 애니메이션 까지 기본옵션으로 구현완료
    - Hero Widget을 이용하여 화면 이동 애니메이션 구현 완료까지.

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -28,36 +28,42 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                )
-              ],
-            ),
-            child: Image.network(
-              // 에러의 원인
+          Hero(
+            tag: {
+              id,
+              title,
+            },
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              child: Image.network(
+                // 에러의 원인
 
-              // 따로 User-Agent 값을 추가하지 않으면 기본값으로 `Dart/<version> (dart:io)` 가 들어갑니다.
-              // (https://api.flutter.dev/flutter/dart-io/HttpClient/userAgent.html)
+                // 따로 User-Agent 값을 추가하지 않으면 기본값으로 `Dart/<version> (dart:io)` 가 들어갑니다.
+                // (https://api.flutter.dev/flutter/dart-io/HttpClient/userAgent.html)
 
-              // 이 값을 지우고 브라우저에서 사용하는 값으로 바꿔줍니다.
-              // (브라우저 값이 아니면 네이버에서 차단하는걸로 보입니다)
+                // 이 값을 지우고 브라우저에서 사용하는 값으로 바꿔줍니다.
+                // (브라우저 값이 아니면 네이버에서 차단하는걸로 보입니다)
 
-              // 방법 1. (local? override)
-              // [home_screen.dart]
-              // headers에 useragent 추가
-              thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+                // 방법 1. (local? override)
+                // [home_screen.dart]
+                // headers에 useragent 추가
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
           const SizedBox(
